7b85f938097a9030096c6b495538a7951fff7d99
Prabodashan <prabodashan14@gmail.com>
2023-03-17 19:47:32 +0530

    added rss to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
+  site: "https://astro-cha-blog.netlify.app",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    sitemap(),
   ],
 });
